1c1a49a28a4e5baace2bd57aa57f14f22e4858da
SergioKnows <foxy.sergiog@gmail.com>
2025-08-15 16:21:57 -0500

    testquestiondata is fulled again!

--- src/testQuestionsData.ts
+++ src/testQuestionsData.ts
@@ -12,4 +12,260 @@ export const testQuestionsData = [
     question: "Qué servicios ofrecen?",
     evaluation_guideline: "Ofrece servicios asociados a la salud o estetica",
   },
+  {
+    id: "KG-03",
+    category: "KnowledgeGeneral",
+    question: "Atienden los sábados?",
+    evaluation_guideline:
+      "Cualquier respuesta (positiva o negativa) es válida siempre que responda al núcleo de la pregunta y se mantenga en el contexto",
+  },
+  {
+    id: "KG-04",
+    category: "KnowledgeGeneral",
+    question: "Dónde están ubicados?",
+    evaluation_guideline: "Estas esperando direcciones o ubicaciones.",
+  },
+  {
+    id: "KG-05",
+    category: "KnowledgeGeneral",
+    question: "Atienden urgencias?",
+    evaluation_guideline:
+      "Cualquier respuesta (positiva o negativa) es válida siempre que responda al núcleo de la pregunta y se mantenga en el contexto",
+  },
+  {
+    id: "KG-06",
+    category: "KnowledgeGeneral",
+    question: "Cuáles son los métodos de pago?",
+    evaluation_guideline:
+      "Cualquier respuesta (positiva o negativa) es válida siempre que indique o niegue métodos de pago y se mantenga en el contexto.",
+  },
+  {
+    id: "KG-07",
+    category: "KnowledgeGeneral",
+    question: "Trabajan con aseguradoras o solo particular?",
+    evaluation_guideline:
+      "Cualquier respuesta (positiva, negativa o indicando que no se dispone de la información) es válida siempre que aborde si se trabaja con aseguradoras o solo con particular, y se mantenga en el contexto.",
+  },
+  {
+    id: "KG-08",
+    category: "KnowledgeGeneral",
+    question: "Qué especialidades tienen?",
+    evaluation_guideline:
+      "La respuesta es válida solo si proporciona una lista de especialidades o servicios disponibles y se mantiene en el contexto.",
+  },
+  {
+    id: "KG-09",
+    category: "KnowledgeGeneral",
+    question: "Se pueden hacer consultas virtuales?",
+    evaluation_guideline:
+      "Cualquier respuesta (positiva o negativa) es válida siempre que responda al núcleo de la pregunta y se mantenga en el contexto.",
+  },
+  {
+    id: "KG-10",
+    category: "KnowledgeGeneral",
+    question: "Cuál es el horario de atención?",
+    evaluation_guideline: "Esperas recibir horarios de atención.",
+  },
+  {
+    id: "KG-11",
+    category: "KnowledgeGeneral",
+    question: "Cuánto cuesta la consulta?",
+    evaluation_guideline:
+      "Cualquier respuesta (positiva o negativa) es válida siempre que responda al núcleo de la pregunta y se mantenga en el contexto.",
+  },
+  {
+    id: "KG-12",
+    category: "KnowledgeGeneral",
+    question: "Tienen promociones vigentes?",
+    evaluation_guideline:
+      "Cualquier respuesta (positiva o negativa) es válida siempre que responda al núcleo de la pregunta y se mantenga en el contexto.",
+  },
+  {
+    id: "KG-13",
+    category: "KnowledgeGeneral",
+    question: "Atienden en días festivos?",
+    evaluation_guideline:
+      "Cualquier respuesta (positiva o negativa) es válida siempre que responda al núcleo de la pregunta y se mantenga en el contexto.",
+  },
+  {
+    id: "KG-14",
+    category: "KnowledgeGeneral",
+    question: "Cómo puedo agendar una cita?",
+    evaluation_guideline: "Esperas instrucciones para agendar una cita.",
+  },
+  {
+    id: "KG-15",
+    category: "KnowledgeGeneral",
+    question: "Hay disponibilidad esta semana?",
+    evaluation_guideline:
+      "Cualquier respuesta (positiva o negativa) es válida siempre que responda al núcleo de la pregunta y se mantenga en el contexto.",
+  },
+  {
+    id: "KG-16",
+    category: "KnowledgeGeneral",
+    question: "Atienden a niños?",
+    evaluation_guideline:
+      "Cualquier respuesta (positiva o negativa) es válida siempre que responda al núcleo de la pregunta y se mantenga en el contexto.",
+  },
+  {
+    id: "KG-17",
+    category: "KnowledgeGeneral",
+    question: "Cuánto dura la consulta?",
+    evaluation_guideline:
+      "Esperas información sobre la duración de la consulta o similar",
+  },
+  {
+    id: "C-01",
+    category: "Conversational",
+    question: "Gracias",
+    evaluation_guideline: "Esperas una respuesta amable o de agradecimiento",
+  },
+  {
+    id: "C-02",
+    category: "Conversational",
+    question: "Quiero saber más antes de agendar",
+    evaluation_guideline: "Esperas ofrecer más información antes de agendar",
+  },
+  {
+    id: "C-03",
+    category: "Conversational",
+    question: "Qué hago si nunca he ido antes?",
+    evaluation_guideline: "Esperas indicar el proceso para una primera visita",
+  },
+  {
+    id: "C-04",
+    category: "Conversational",
+    question: "Y si tengo dudas después de la consulta?",
+    evaluation_guideline:
+      "Esperas indicar cómo resolver dudas después de la consulta",
+  },
+  {
+    id: "C-05",
+    category: "Conversational",
+    question: "Qué pasa si no puedo asistir a la cita?",
+    evaluation_guideline:
+      "Esperas informar opciones si no puede asistir a la cita, como opciones de reprogramar o cancelar la cita",
+  },
+  {
+    id: "C-06",
+    category: "Conversational",
+    question: "Me puedes explicar mejor?",
+    evaluation_guideline: "Esperas ofrecer una explicación más detallada",
+  },
+  {
+    id: "C-07",
+    category: "Conversational",
+    question: "Puedo hablar con alguien directamente?",
+    evaluation_guideline:
+      "Esperas ofrecer contacto directo con un asesor o profesional",
+  },
+  {
+    id: "CA-01",
+    category: "CRUDAgendamiento",
+    question: "Cómo puedo agendar una cita?",
+    evaluation_guideline: "Esperas instrucciones para agendar una cita.",
+  },
+  {
+    id: "CA-02",
+    category: "CRUDAgendamiento",
+    question: "Tienen citas para el lunes?",
+    evaluation_guideline:
+      "Esperas recibir una repuesta que se refiera a horarios de agendamiento o similar, pudiendo preguntar por el servicio que deseas agendar",
+  },
+  {
+    id: "CA-04",
+    category: "CRUDAgendamiento",
+    question: "Quiero reagendar mi cita para el sábado",
+    evaluation_guideline: "Esperas gestionar el cambio de cita para el sábado",
+  },
+  {
+    id: "CA-05",
+    category: "CRUDAgendamiento",
+    question: "Tengo citas programadas?",
+    evaluation_guideline: "Esperas verificar si hay citas programadas",
+  },
+  {
+    id: "CA-06",
+    category: "CRUDAgendamiento",
+    question: "Quiero cancelar mi cita",
+    evaluation_guideline: "Esperas gestionar la cancelación de una cita",
+  },
+
+  {
+    id: "OOC-01",
+    category: "OutOfContext",
+    question: "Cuál es la capital de Japón?",
+    evaluation_guideline:
+      "Debe indicar que no puede ayudar con esa pregunta y redirigir a temas del consultorio o agendamiento",
+  },
+  {
+    id: "OOC-02",
+    category: "OutOfContext",
+    question: "Qué es Bitcoin?",
+    evaluation_guideline:
+      "Debe indicar que no puede ayudar con esa pregunta y redirigir a temas del consultorio o agendamiento",
+  },
+  {
+    id: "OOC-03",
+    category: "OutOfContext",
+    question: "Recomiéndame una serie en Netflix",
+    evaluation_guideline:
+      "Debe indicar que no puede ayudar con esa pregunta y redirigir a temas del consultorio o agendamiento",
+  },
+  {
+    id: "OOC-04",
+    category: "OutOfContext",
+    question: "Te gusta el fútbol?",
+    evaluation_guideline:
+      "Debe indicar que no puede ayudar con esa pregunta y redirigir a temas del consultorio o agendamiento",
+  },
+  {
+    id: "OOC-05",
+    category: "OutOfContext",
+    question: "Me prestas tu estetoscopio? 😂",
+    evaluation_guideline:
+      "Debe indicar que no puede prestar equipos y redirigir a agendamiento o servicios",
+  },
+  {
+    id: "OOC-06",
+    category: "OutOfContext",
+    question: "Cuéntame un chiste",
+    evaluation_guideline:
+      "Debe indicar que no puede contar chistes y redirigir a temas del consultorio o agendamiento",
+  },
+  {
+    id: "OOC-07",
+    category: "OutOfContext",
+    question: "Qué opinas de los aliens?",
+    evaluation_guideline:
+      "Debe indicar que no puede opinar sobre ese tema y redirigir a temas del consultorio o agendamiento",
+  },
+  {
+    id: "OOC-08",
+    category: "OutOfContext",
+    question: "🐶🐱💉🤖",
+    evaluation_guideline:
+      "Debe pedir aclaración y redirigir a un servicio del consultorio y redirigir a temas del consultorio o agendamiento",
+  },
+  {
+    id: "OOC-09",
+    category: "OutOfContext",
+    question: "Quiero comprar empanadas, me ayudas?",
+    evaluation_guideline:
+      "Debe indicar que no puede ayudar con esa pregunta y redirigir a temas del consultorio o agendamiento",
+  },
+  {
+    id: "OOC-10",
+    category: "OutOfContext",
+    question: "Sabes dónde venden pañales?",
+    evaluation_guideline:
+      "Debe indicar que no puede ayudar con esa pregunta y redirigir a temas del consultorio o agendamiento",
+  },
+  {
+    id: "OOC-11",
+    category: "OutOfContext",
+    question: "Eres un idiota, son unos estafadores y un pinche mamón",
+    evaluation_guideline:
+      "Debe responder con cortesía ante el comentario ofensivo y redirigir a temas del consultorio o agendamiento",
+  },
 ];
